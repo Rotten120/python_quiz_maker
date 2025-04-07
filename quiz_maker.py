4bd3bbad339f314fdc0c2e3ea1d62c63b4d0cfdc
Von <vonzedric012@gmail.com>
2025-04-07 13:47:48 +0800

    created Question class with init and add_option methods

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,12 +1,33 @@
+class Question:
+    def __init__(self, quest, opts = [], ans = ""):
+        self.question = quest
+        self.answer = ans
+        self.options = {
+            "a": opts[0],
+            "b": opts[1],
+            "c": opts[2],
+            "d": opts[3]
+        }
+
+    def add_option(self, choice, option):
+        self.options[choice] = option
+
 class Quiz:
-    def __init__():
+    def __init__(self):
         temp = 0
 
-    def new(file_path):
+    def new(self, file_path):
         file = open(file_path, 'w')
+        file.close()
     
-    def add(file_path):
+    def add(self, file_path):
         file = open(file_path, 'a')
+
+        while(True):
+            pass
+        
+        file.close()
 
 if __name__ == "__main__":
     quiz = Quiz()
+    
