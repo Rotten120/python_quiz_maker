61a908ff8afea6a34ed86dd5cab0dc7642f83070
Von <vonzedric012@gmail.com>
2025-04-07 13:39:44 +0800

    initial commit

---
+++ quiz_maker.py
@@ -0,0 +1,12 @@
+class Quiz:
+    def __init__():
+        temp = 0
+
+    def new(file_path):
+        file = open(file_path, 'w')
+    
+    def add(file_path):
+        file = open(file_path, 'a')
+
+if __name__ == "__main__":
+    quiz = Quiz()
