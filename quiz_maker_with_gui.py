3733f0f664c1bd247aff00554111c0347087a3c1
Von <vonzedric012@gmail.com>
2025-04-09 18:46:47 +0800

    added new_quiz_mene and DRAW elements

--- quiz_maker_with_gui.py
+++ quiz_maker_with_gui.py
@@ -1,16 +1,56 @@
 from tkinter import *
 import quiz_maker as q_maker
+import os
 
 bg_color = "#1a1a1a"
-txt_color = "white"
 title_font = ("Arial", 40, "bold")
+subtitle_font = ("Arial", 15, "bold")
 button_font = ("Arial", 15)
-button_color = "#333333"
+
+txt_color = "white"
+dark_color = "#333333"
 
 tk = Tk()
 
-def q():
-    print("LOL")
+def clear_screen():
+    for widget in tk.winfo_children():
+        widget.destroy()
+
+def check_file_dir(file_path):
+    
+
+def new_quiz_menu():
+    clear_screen()
+    
+    subtitle = Label(
+        tk,
+        text = "Input file name",
+        font = subtitle_font,
+        background = bg_color,
+        foreground = txt_color
+    )
+
+    inp_file = Entry(
+        tk,
+        font = ("Arial", 10),
+        width = 30,
+        background = dark_color,
+        foreground = txt_color
+    )
+
+
+    confirm = Button(
+        tk,
+        text = "Confirm",
+        font = button_font,
+        background = dark_color,
+        foreground = txt_color
+        command = lambda: check_file_dir(inp_file.get())
+    )
+    
+    subtitle.place(relx = 0.2, rely = 0.05, anchor = "center")
+    inp_file.place(relx = 0.29, rely = 0.12, anchor = "center")
+    confirm.place(relx = 0.88, rely = 0.94, anchor = "center")
 
 def window_menu():
     title = Label(
@@ -25,18 +65,18 @@ def window_menu():
         tk,
         text = "Create New Quiz",
         font = button_font,
-        bg = button_color,
+        bg = dark_color,
         fg = txt_color,
-        command = q
+        command = new_quiz_menu
     )
 
     edit_quiz = Button(
         tk,
         text = "Edit Existing Quiz",
         font = button_font,
-        bg = button_color,
+        bg = dark_color,
         fg = txt_color,
-        command = q
+        command = new_quiz_menu
     )
 
     title.place(relx = 0.5, rely = 0.3, anchor = "center")
