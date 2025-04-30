8599360ff6a83300c322a3758f22e74da956aefb
Von <vonzedric012@gmail.com>
2025-04-30 17:09:21 +0800

    encapsulated base scripts in preparation for a 'class-based' codebase

--- base_file.py
+++ base_file.py
@@ -4,4 +4,32 @@ import ele_pre_sets as pre_set
 def clear_screen(tk):
     for widget in tk.winfo_children():
         widget.destroy()
-        
+
+#window/s in obj refers to the same thing as page/s in parameters
+class Quiz_Maker(Tk):
+    def __init__(self, title = "tkinter", size = "500x500", pages = {}):
+        super().__init__()
+        self.title(title)
+        self.geometry(size)
+        self.windows = pages
+
+    def add_window(self, page, bg_color = "white"):
+        window = page(self, bg_color)
+        self.windows[page] = window
+        window.place(relwidth = 1, relheight = 1)
+
+    def set_window(self, page):
+        window = self.windows[page]
+        window.tkraise()
+
+#base class to be inherited by windows to be made
+class Window(Frame):
+    def __init__(self, master, bg_color = "white"):
+        super().__init__(master)
+        self.parent = master
+        self.configure(bg = bg_color)
+        self.display()   
+
+    def display(self):
+        txt = pre_set.label2(self, "SAMPLE WINDOW")
+        txt.pack()
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from tkinter_gui import *
+from base_file import *
 import menu_window as menu
+import create_new_file_window as new_file
+import add_questions_window as add_quest
 
-tk = Tk()
-width = 400
-height = 400
-        
 if __name__ == "__main__":
-    tk.title("Python Quiz Maker")
-    tk.geometry(str(width) + "x" + str(height))
+    quiz_maker = Quiz_Maker(
+        "Python Quiz Maker",
+        "400x400"
+    )
     
-    tk.configure(bg = pre_set.bg_color)
-    menu.window(tk)
-    tk.mainloop()
+    windows = (
+        #put all windows in here
+        #first window must be the 'main menu'
+    )
+
+    for window in windows:
+        quiz_maker.add_window(window, pre_set.bg_color)
+    
+    quiz_maker.set_window(windows[0])
+    quiz_maker.mainloop()
+
+    print("MAIN")
