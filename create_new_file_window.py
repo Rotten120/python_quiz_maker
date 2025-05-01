47cd874f22108d556e4f3ef9589d3736f870ff7e
Von <vonzedric012@gmail.com>
2025-05-01 11:51:52 +0800

    adjusted the size and position of elements based on the new sie of the Sub_Screen made for this script, also encapslate some code into functions to prevent repetition

--- create_new_file_window.py
+++ create_new_file_window.py
@@ -1,39 +1,46 @@
 from base_file import *
 import add_questions_window as aq
 import os
-
+        
 class New_File(Window):
     def display(self):
-        sub_txt = "Input file name"
-        subtitle = pre_set.label1(self, sub_txt)
-
-        inp_file = pre_set.entry1(self, 30)
+        inp_file = pre_set.entry1(self, 28)
         
         conf_txt = "Confirm"
-        confirm = pre_set.button1(
+        confirm = pre_set.button3(
             self, conf_txt, lambda: self.check_file_dir(inp_file.get())
         )
+
+        confirm.configure(width = 10)
         
-        subtitle.place(relx = 0.2, rely = 0.05, anchor = "center")
-        inp_file.place(relx = 0.29, rely = 0.12, anchor = "center")
-        confirm.place(relx = 0.88, rely = 0.94, anchor = "center")
+        inp_file.place(x = 26, y = 30)
+        confirm.place(x = 85, y = 60)
 
         print("CREATE")
 
     def check_file_dir(self, file_path):
+        def get_master_window():
+            #SELF -> CREATE_NEW_FILE_WINDOW
+            #SELF.PARENT -> SUB_SCREEN MADE
+            #SELF.PARENT.PARENT -> MAIN_WINDOW
+            #SELF.PARENT.PARENT.PARENT -> QUIZ_MAKER
+            return self.parent.parent.parent
+        
+        def draw_notice_lbl(text):
+            notice = pre_set.label2(self, text, "red")
+            notice.configure(width = 20, anchor = "w")
+            notice.place(x = 26, y = 8)
+        
         if file_path == "":
-            lbl_txt = "* Invalid file name"
-            notice = pre_set.label2(self, lbl_txt, "red")
-            notice.place(relx = 0.03, rely = 0.15)
+            draw_notice_lbl("* Invalid file name")
             return
         elif not file_path.endswith(".txt"):
             file_path += ".txt"
 
         if os.path.exists(file_path):
-            lbl_txt = "* File already exists"
-            notice = pre_set.label2(self, lbl_txt, "red")
-            notice.place(relx = 0.03, rely = 0.15)
+            draw_notice_lbl("* File already exists")
         else:
-            self.parent.windows[aq.Add_Questions].set_file_path(file_path)
-            self.parent.set_window(aq.Add_Questions)
+            get_master_window().windows[aq.Add_Questions].set_file_path(file_path)
+            get_master_window().parent.set_window(aq.Add_Questions)
+            self.parent.destroy()
 
